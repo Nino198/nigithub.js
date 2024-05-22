fa0ab8de3a1d5c5a878f688adf3cdae879dd3b7c
Nino198 <151477098+Nino198@users.noreply.github.com>
2024-05-23 02:45:21 +0400

    lesen1

---
+++ script.js
@@ -0,0 +1,13 @@
+userNumber=100;
+userNumber=100 >50;
+console.log(usernume=50);
+userNumber!=50;
+userName=mariami;
+usernume =true;
+userNume!= FontFaceSetLoadEvent;
+console.log(userNume=true);
+swichNume=true;
+swicNume!=FontFaceSetLoadEvent;
+
+
+
